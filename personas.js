9711192790cebbf34382928452d341a61fe7ec84
Heber Agustin Carrizo <waper74@gmail.com>
2026-06-22 23:57:42 -0300

    Funcion para calcular y clasificar IMC

--- personas.js
+++ personas.js
@@ -3,3 +3,22 @@ let contadorId = 0;
 
 const formulario = document.getElementById("form-personas");
 const cuerpoTabla = document.getElementById("cuerpo-tabla-personas");
+
+function calcularIMC(peso, altura) {
+  const imc = peso / (altura * altura);
+  return imc.toFixed(1); // redondeo a 1 decimal
+}
+
+function clasificarIMC(imc) {
+  const valor = parseFloat(imc);
+
+  if (valor < 18.5) {
+    return "Bajo peso";
+  } else if (valor < 25) {
+    return "Normal";
+  } else if (valor < 30) {
+    return "Sobrepeso";
+  } else {
+    return "Obesidad";
+  }
+}
